4f7ba8abcccbea2801dd6ddc698643b91f701686
katharina jäger <jaeger.kt@gmail.com>
2022-01-17 17:04:50 +0100

    Add stying, try to add input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 import { useState } from 'react';
-import AskForInput from './AskForInput';
 
 const Container = styled.div`
   margin: 200px;
@@ -9,6 +8,12 @@ const Div = styled.div`
   padding: 100px;
   font-size: 3em;
   border-radius: 30px;
+`;
+const Button = styled.button`
+  border-radius: 20px;
+  margin-top: 50px;
+  font-size: 2em;
+  border: 3px solid black;
 `;
 
 export default function App() {
@@ -23,14 +28,13 @@ export default function App() {
       >
         Generated Color: #{color}
       </Div>
-      <button
+      <Button
         onClick={() =>
           setColor(Math.floor(Math.random() * 16777215).toString(16))
         }
       >
         Generate
-      </button>
-      <AskForInput />
+      </Button>
     </Container>
   );
 }
--- src/AskForInput.js
+++ src/AskForInput.js
@@ -1,17 +1,32 @@
+import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 export default function AskForInput() {
   const [hue, setHue] = useState('');
-
+  const [luminosity, setLuminosity] = useState('');
   return (
     <div>
       <label htmlFor="hue">Please enter a color hue: </label>
       <br />
-      <input type="text" id="hue" value={hue} />
+      <input
+        type="text"
+        id="hue"
+        onChange={(event) => setHue(event.target.value)}
+      />
       <br />
-      <button onClick={() => setHue(hue)} onClick={() => randomized(hue, 0.4)}>
-        Generate a color based on my hue!
-      </button>
+      <label htmlFor="luminosity">Please enter the desired luminosity: </label>
+      <input
+        type="text"
+        id="luminosity"
+        onChange={(event) => setLuminosity(event.target.value)}
+      />
+      <br />
+      <button>Set hue and luminosity</button>
+      <div
+        style={{
+          backgroundColor: randomColor({ hue: hue, luminosity: luminosity }),
+        }}
+      ></div>
     </div>
   );
 }
